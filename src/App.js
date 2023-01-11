11c05c07851c69e6416133eeb926acb1bd5759e9
AndrwsFaith <64641309+Morifeoluwa@users.noreply.github.com>
2023-01-11 22:18:10 +0100

    abc

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./assets/css/grid.css";
+import "./assets/css/style.css";
+
+//importing english pages
+import { About, Contact, Home, NotFound, Projects } from "./en/index";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/en/about" element={<About />} />
+          <Route exact path="/en/contact" element={<Contact />} />
+          <Route exact path="/en/project" element={<Projects />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
